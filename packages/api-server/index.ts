fe5b6c4032fcc920a22fb97a59339c64e5d924ae
Mowalky <dev@mowalky.com>
2022-07-23 14:45:01 -0500

    trpc router query

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
 import * as trpc from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
 
-const appRouter = trpc.router();
+const appRouter = trpc.router().query("hello", {
+  resolve() {
+    return "Hello world!";
+  },
+});
 
 const app = express();
 const port = 8080;
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => null,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from api-server");
